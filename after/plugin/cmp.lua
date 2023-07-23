618382daa3778219a9f9418293ad81df98c12d4c
22040953-Zhang Qingyu <zqybegin@gmail.com>
2023-07-23 09:07:39 +0800

    change tab only luasnip expand

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -65,9 +65,10 @@ cmp.setup {
         -- Set `select` to `false` to only confirm explicitly selected items.
         ["<CR>"] = cmp.mapping.confirm { select = true },
         ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expandable() then
+            -- if cmp.visible() then
+            --     cmp.select_next_item()
+            -- elseif luasnip.expandable() then
+            if luasnip.expandable() then
                 luasnip.expand()
             elseif luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
@@ -78,10 +79,13 @@ cmp.setup {
             end
         end, {"i","s",}),
         ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
+            -- if cmp.visible() then
+            --     cmp.select_prev_item()
+            -- elseif luasnip.jumpable(-1) then
+            if luasnip.jumpable(-1) then
                 luasnip.jump(-1)
+            elseif luasnip.expand_or_jumpable(-1) then
+                luasnip.expand_or_jump(-1)
             else
                 fallback()
             end
