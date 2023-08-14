dc443858f777b9b7cba6dff96fd6ca1ca69eb8aa
22040953-Zhang Qingyu <zqybegin@gmail.com>
2023-08-14 08:53:44 +0800

    lazy: cancel notification when new updates are found

--- lua/core/Lazy.lua
+++ lua/core/Lazy.lua
@@ -59,8 +59,13 @@ require("lazy").setup({
         -- automatically check for plugin updates
         enabled = false,
         concurrency = nil, ---@type number? set to 1 to check for updates very slowly
-        notify = true,    -- get a notification when new updates are found
+        notify = false,    -- get a notification when new updates are found
         frequency = 3600, -- check for updates every hour
+    },
+    change_detection = {
+        -- automatically check for config file changes and reload the ui
+        enabled = false,
+        notify = false, -- get a notification when changes are found
     },
     ui = {
         -- a number <1 is a percentage., >1 is a fixed size
